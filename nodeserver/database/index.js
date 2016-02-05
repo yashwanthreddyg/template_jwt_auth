c10ffece322382f36d00cdd05582a22255947818
Yashwanth Gondi <yashreddy.g@gmail.com>
2016-02-05 14:28:21 +0530

    added db methods to validate user and get users by uname

--- nodeserver/database/index.js
+++ nodeserver/database/index.js
@@ -1,6 +1,53 @@
 var database = {};
-database.getUsers = function() {
-    return ['yashwanth', 'surya'];
+var mock = [{
+    _id: 'abcd1',
+    username: 'username1',
+    password: 'password1'
+}, {
+    _id: 'abcd2',
+    username: 'username2',
+    password: 'password2'
+}, {
+    _id: 'abcd3',
+    username: 'username3',
+    password: 'password3'
+}];
+database.getUsers = function(cb) {
+    cb(mock);
+};
+database.findUserByUsername = function(uname, cb) {
+    var user = undefined;
+    for (var i in mock) {
+        if (mock[i].username == uname) {
+            user = mock[i];
+            break;
+        }
+    }
+    if (!user) {
+        cb(null, {
+            message: 'user not found with uname \'' + uname + '\''
+        });
+        return;
+    } else {
+        cb(user, null);
+    }
 };
 
+database.validateUser(uname, pswd, scb, ecb) {
+    database.findUserByUsername(uname, function(res, err) {
+        if (err) {
+            ecb({
+                message: 'User not found'
+            });
+        } else {
+            if (res.password == pswd) {
+                scb();
+            } else {
+                ecb({
+                    message: 'wrong password'
+                });
+            }
+        }
+    });
+}
 module.exports = database;
--- nodeserver/routes/authentication/index.js
+++ nodeserver/routes/authentication/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var config = require('../../config.js');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+var db = require('../../database');
 router.post('/', function(req, res) {
+    if(req.body.username && req.body.password){
+        db.validateUser(req.body.username,req.body.password,null,null);
+    }
     if (req.body.username == 'username' && req.body.password == 'password') {
         var secret = config.appsecret;
         var token = jwt.sign("validuser", secret, {
